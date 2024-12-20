0904d9034a33a0557fec369d4ab55b5832c80a3b
ImaneTech <imane.zouine100@gmail.com>
2024-12-20 11:15:49 +0100

    Cycle 1 - Test 1 : test echoue (RED) --> Failed

--- src/main/java/exercices/calculator/ExpressionEvaluator.java
+++ src/main/java/exercices/calculator/ExpressionEvaluator.java
@@ -2,4 +2,8 @@ package exercices.calculator;
 
 
 public class ExpressionEvaluator {
+   public static int evaluate(String expression) {
+       int res=-1;  //expression incorrect
+       return res;
+   }
 }
--- src/test/java/exercices/calculator/ExpressionEvaluatorTest.java
+++ src/test/java/exercices/calculator/ExpressionEvaluatorTest.java
@@ -1,7 +1,17 @@
 package exercices.calculator;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class ExpressionEvaluatorTest {
+
+    @Test
+    public void return_0_pour_chaine_vide() {
+        //GIVEN
+        int expectedResult =0;
+        //WHEN
+        int actualResult = ExpressionEvaluator.evaluate("");
+        //THEN
+        assertThat(actualResult).isEqualTo(expectedResult);
+    }
 
 }
